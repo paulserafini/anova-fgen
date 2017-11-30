71ad8f552f106309f87b61aa6cc2be769a731c6d
Paul Serafini <pserafin@sfu.ca>
2017-11-29 19:05:23 -0800

    Remove the provisional column if it's empty

--- instructional/tablegen.R
+++ instructional/tablegen.R
@@ -1,6 +1,10 @@
 function (table, length) {
   cat('<table border="1" bordercolor="808080"')
-  cat('<tr><td align="center"><font color=white>E(MS)</font></td><td align="center" colspan="2"><b>Constant</b></td><td align="center" colspan="',length-3,'"><b>Provisional</b></td></tr>')
+  if (length > 3) {
+  	cat('<tr><td align="center"><font color=white>E(MS)</font></td><td align="center" colspan="2"><b>Constant</b></td><td align="center" colspan="',length-3,'"><b>Provisional</b></td></tr>')
+  } else {
+  	cat('<tr><td align="center">&nbsp;</td><td align="center" colspan="2"><b>Constant</b></td></tr>')
+  }
     for (i in 1:nrow(table)) {
 			cat('<tr>')
 			for (j in 1:length(table[i,])) {
