8515945be1183da720664823c279058df0195c87
Paul Serafini <pserafin@sfu.ca>
2017-11-30 21:27:11 -0800

    Update instructional version

--- instructional/test.R
+++ instructional/test.R
@@ -1,96 +1,90 @@
 tablegen <- dget("tablegen.R")
 function (factors, factor_type, step) {
+# factors is a vector of letters, e.g. c("A", "B", "C")
+# factor_type is a vector of 1s and 0s where 1 = fixed and 0 = random, e.g. c(1, 0, 1)
 
   	# Generate vector of 2 way, 3 way, etc. combinations of input
 	combogen <- function (input, num_factors) {
 	  	output <- c()
 		for (i in 1:num_factors) {
-		  matrix <- combn(input, i)
-		  vector <- lapply(1:ncol(matrix), function(i) matrix[,i])
-		  output <- c(output, vector)
+		  matt <- combn(input, i)
+		  vect <- lapply(1:ncol(matt), function(i) matt[,i])
+		  output <- c(output, vect)
 		}
 		return(output)
 	}
 
+	# Make vectors of the effects and their types
 	num_factors <- length(factors)
-	factor_df <- unlist(lapply(factors, function(v) paste0('(',tolower(v), '-1)')))
-
-	# Make vectors of the effects, their type (0 = random, 1 = fixed), and their degrees of freedom
 	effects <- combogen(factors, num_factors)
 	effect_type <- combogen(factor_type, num_factors)
-	effect_df <- combogen(factor_df, num_factors)
-
-	effect_df <- lapply(effect_df, function(i) paste(i, collapse=""))
 	effect_type <- lapply(effect_type, function(effect) prod(effect))
 
-	# Create denominator for random effects
-	num_effects <- length(effects)
-	levels <- lapply(1:num_effects, function(i) paste(effects[[i]], collapse=""))
-	levels <- unlist(lapply(levels, function(v) tolower(v)))
+	# Make denominator for fixed effects
+	factor_df <- lapply(factors, function(v) paste0("(",tolower(v), "-1)"))
+	fixed_denominator <- combogen(factor_df, num_factors)
+	fixed_denominator <- lapply(fixed_denominator, function(i) paste(i, collapse = ""))
 
-	# Create vector of denominators
-	combined <- list(levels, effect_df)
-	denominators <- unlist(lapply(1:num_effects, function(x) combined[[ effect_type[[x]]+1 ]] [[x]] ))
+	# Create numerator for fixed effects
+	fixed_numerator <- paste(factors, collapse = "")
+	fixed_numerator <- tolower(fixed_numerator)
 
-	# Create numerator
-	numerator <- tolower(paste(factors, collapse=""))
-	numerator <- paste0('n',numerator)
-	numerators <- rep(numerator, num_effects)
-	
 	# Create weights
+	num_effects <- length(effects)
 	weights <- c()
-	for (i in 1:num_effects) { # step 3
-	  if (step > 2) {
-  		if (effect_type[[i]] == 0) { # remove common characters from numerator and denominator; if denominator is empty then weight = numerator
-  			num_pretrim <- unlist(strsplit(numerators[[i]], ""))
-  			den_pretrim <- unlist(strsplit(denominators[[i]], ""))
-  			if (step > 3) { # step 4
-    			num_trim <- paste(num_pretrim[!(num_pretrim %in% den_pretrim)], collapse="")
-    			den_trim <- paste(den_pretrim[!(den_pretrim %in% num_pretrim)], collapse="")
-  			} else {
-  			  num_trim <- paste(num_pretrim, collapse="")
-  			  den_trim <- paste(den_pretrim, collapse="")
-  			}
-  			if (den_trim == '') {
-  			  weight <- num_trim
-  			} else {
-  			  weight <- paste0('[ <sup>', num_trim, '</sup>&frasl;<sub>', den_trim, '</sub> ]')
-  			}
-  		} else {
-  			numerator <- numerators[[i]]
-  			denominator <- denominators[[i]]
-  			weight <- paste0('[ <sup>', numerator, '</sup>&frasl;<sub>', denominator, '</sub> ]')
-  		}
-	  } else {
-	    weight <- '______'
-	  }
+	for (i in 1:num_effects) {
+	  	if (step > 2) {
+			if (effect_type[[i]] == 0) {
+
+  				if (step > 3) { # step 4
+					numerator <- factors[!(factors %in% effects[[i]])]
+					denominator <- effects[[i]][!(effects[[i]] %in% factors)]
+				} else {
+					numerator <- factors
+					denominator <- effects[[i]]
+				}
+
+					numerator <- paste(numerator, collapse = "")
+					numerator <- tolower(numerator)
+
+					denominator <- paste(denominator, collapse = "")
+					denominator <- tolower(denominator)
+
+				if (denominator == "") {
+				  weight <- paste0("n", numerator)
+				} else {
+				  weight <- paste0("[ <sup>n", numerator, "</sup>&frasl;<sub>", denominator, "</sub> ]")
+				}
+
+			} else {
+				weight <- paste0("[ <sup>n", fixed_numerator, "</sup>&frasl;<sub>", fixed_denominator[[i]], "</sub> ]")
+			}
+		} else {
+	    	weight <- '______'
+		}
 		weights[[i]] <- weight
 	}
 
-
-	effect_strings <- lapply(effects, function(x) paste(x, collapse=""))
-
 	# Assemble weights and variances
-	weightplusvariance <- lapply(1:num_effects, function(x) {
-		paste0(weights[[x]],'&sigma;<sub>',effect_strings[[x]],'</sub>')
-	})
-
-	provisional <- matrix("", nrow=1, ncol=num_effects)
+	effect_strings <- lapply(effects, function(x) paste(x, collapse = ""))
+	weightplusvariance <- paste0(weights, "&sigma;<sub>", effect_strings, "</sub>")
+	
+	# Create provisionals for each effect
+	provisional <- matrix("", nrow = 0, ncol = num_effects)
 	for (a in 1:num_effects) {
 
-		matt <- cbind(weightplusvariance,tolower(effect_strings))
+		matt <- cbind(weightplusvariance, tolower(effect_strings))
 		matt <- matt[-a,]
 
 		# Rule out effects w/ out the focal effects
 		for (e in effects[[a]]) {
 			char <- tolower(e)
-			matt <- matt[ grepl(char, matt[,2]),, drop=FALSE ]
+			matt <- matt[grepl(char, matt[,2]),, drop = FALSE]
 			matt[,2] <- gsub(char, "", as.character(matt[,2]))
 		}
-		#matt <- matt[ matt[,2] != "",, drop=FALSE ]
 
 		# Remove fixed effects
-		alphabet <- c('a','b','c','d')
+		alphabet <- c("a","b","c","d")
 		if (step > 1) { # step 2
 			for (f in factors) {
 				char <- tolower(f)
@@ -98,21 +92,26 @@ function (factors, factor_type, step) {
 				effect <- effect_type[[num]]
 				matt[,2] <- gsub(char, effect, as.character(matt[,2]))
 			}
-  			matt[ grepl(1, matt[,2]), ] <- paste0('<font color=white>',matt[ grepl(1, matt[,2]), ],'</font>')
+			matt[ grepl(1, matt[,2]), ] <- paste0('<font color=white>',matt[ grepl(1, matt[,2]), ],'</font>')
 		}
 
+		# Pad vector and add it to provisionals matrix
 		diff <- num_effects - nrow(matt)
 		vect <- c(matt[,1], rep("", diff))
 		provisional <- rbind(provisional, vect)
 	}
 
-
 	# Format output
-	emsu <- paste0('E(MS<sub>U/', paste(factors, collapse=""),'</sub>)')
-	row_names <- c(emsu, lapply(effect_strings, function(x) paste0('E(MS<sub>', x, '</sub>)')))
-	weights <- c("", paste(weights, '&sigma;<sub>', effect_strings, '</sub>', sep=''))
-	output <- cbind(row_names, c('&nbsp;&nbsp;&sigma;&nbsp;&nbsp;'), weights, provisional)
+	row_names <- paste0("E(MS<sub>", effect_strings, "</sub>)")
+	output <- cbind(row_names, "&nbsp;&nbsp;&sigma;&nbsp;&nbsp;", weightplusvariance, provisional)
+
+	factors_string <- paste(factors, collapse = "")
+ 	top_row_name <- paste0("E(MS<sub>U/", factors_string, "</sub>)")
+	top_row_padding <- rep("", num_effects + 1)
+	top_row <- c(top_row_name, "&sigma;", top_row_padding)
+	output <- rbind(top_row, output)
+
 	output <- output[, colSums(output == "") != nrow(output)]
 
-	tablegen(output,ncol(output))
+	tablegen(output,nrow(output))
 }
